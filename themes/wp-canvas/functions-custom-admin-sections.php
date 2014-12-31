608f34575131e63c7e45eb5c86d66839d29eb4b0
Ind <mailtokasun@gmail.com>
2014-12-31 14:59:32 +0530

    Custom Admin Page

---
+++ themes/wp-canvas/functions-custom-admin-sections.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * register settings
+ */
+function theme_settings_init()
+{
+    register_setting( 'theme_settings', 'theme_settings' );
+}
+ 
+/*
+ * add a sample page to menu
+----------------------------------------------------*/
+function add_page_sample1()
+{
+    $page_title = 'Sample Admin Page 1';
+    $menu_title = 'Sample Admin Page 1';
+    $capability = 'manage_options';
+    $menu_slug = 'sample-admin-page1';
+    $function = 'fn_page_sample1';
+    $icon_url = '';
+    $position = '45';
+        
+    add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+}
+
+function add_page_sample2()
+{
+	$page_title = 'Sample Admin Page 2';
+    $menu_title = 'Sample Admin Page 2';
+    $capability = 'manage_options';
+    $menu_slug = 'sample-admin-page2';
+    $function = 'fn_page_sample2';
+    $icon_url = '';
+    $position = '46';
+        
+    add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+}
+ 
+/*
+ * add actions
+ */
+add_action( 'admin_init', 'theme_settings_init' );
+add_action( 'admin_menu', 'add_page_tour_req' );
+add_action( 'admin_menu', 'add_page_admissions' );
+ 
+/*
+ * Page Outputs
+*/
+function fn_page_sample1() {
+    ?>
+     <div class="wrap">
+         
+         <div id="icon-options-general"></div>
+         
+         <h2>Sample Page 1 Title</h2>
+         
+     </div> <!--/wrap-->
+     <?php
+}
+
+function fn_page_sample2() {
+    ?>
+     <div class="wrap">
+         
+         <div id="icon-options-general"></div>
+         
+         <h2>Sample Page 2 Title</h2>
+         
+     </div> <!--/wrap-->
+     <?php
+}
+
+// EOF.
--- themes/wp-canvas/functions.php
+++ themes/wp-canvas/functions.php
@@ -46,5 +46,6 @@ include_once('functions-admin.php');
 include_once('functions-custom.php');
 include_once('functions-theme-supports.php');
 include_once('functions-email-setup.php');
+include_once('functions-custom-admin-sections.php');
 
 ?>
